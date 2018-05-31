a253bfda497631428c7f156849e14332bf0c681e
Marcelo Dewes <marcelo@marcelo-note.localdomain>
2018-05-31 22:38:31 +0000

    Aplicando máscara na exibição do CPF

--- app/Usuario.php
+++ app/Usuario.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    //
+    public function getCpfAttribute()
+    {
+        return vsprintf('%s%s%s.%s%s%s.%s%s%s-%s%s', str_split($this->attributes['cpf']));
+    }
 }
