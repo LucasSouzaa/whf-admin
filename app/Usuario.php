595deb1c02963069728deaa686d515020f5a1229
Marcelo Dewes <marcelo@marcelo-note.localdomain>
2018-06-02 02:31:52 +0000

    Melhorias no model

--- app/Usuario.php
+++ app/Usuario.php
@@ -3,11 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Usuario extends Model
 {
+    protected $fillable = ['nome', 'email', 'nascimento', 'cpf', 'senha', 'foto'];
+
     public function getCpfAttribute()
     {
         return vsprintf('%s%s%s.%s%s%s.%s%s%s-%s%s', str_split($this->attributes['cpf']));
+    }
+
+    public function getNascimentoAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['nascimento'])->format('d/m/Y');
     }
 }
